286bc6814497399a24aa7c2fa795c13881b17748
rubyspring <elavery1@madisoncollege.edu>
2013-02-04 14:22:51 -0600

    Demo app working?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
-    has_many :microposts
+  has_many :microposts
 
   validates :name, :length => { :minimum => 3 }
   validates :email, :length => { :minimum => 5 }
